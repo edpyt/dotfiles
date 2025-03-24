a17db2bc3abaac1155363eaf1fd7ad2dcff00727
edpyt <edrsa1321s@gmail.com>
2025-03-24 09:05:26 +0500

    feat(nvim): add neovide configuration

---
+++ .config/nvim/lua/plugins/neovide.lua
@@ -0,0 +1,22 @@
+if not vim.g.neovide then
+  return {} -- do nothing if not in a Neovide session
+end
+
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    options = {
+      opt = { -- configure vim.opt options
+      },
+      g = { -- configure vim.g variables
+        neovide_cursor_vfx_mode = "wireframe",
+        -- configure padding
+        neovide_padding_top = 0,
+        neovide_padding_bottom = 0,
+        neovide_padding_right = 0,
+        neovide_padding_left = 0,
+      },
+    },
+  },
+}
