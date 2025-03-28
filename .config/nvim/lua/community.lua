94fcfe30374857dddb19befdbb272b29c72941d7
edpyt <edrsa1321s@gmail.com>
2025-03-28 08:58:30 +0500

    feat(nvim): migrate to AstroNvim v5
    woooow! it's so cool now!

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -1,37 +1,23 @@
--- AstroCommunity: import any community modules here
--- We import this file in `lazy_setup.lua` before the `plugins/` folder.
--- This guarantees that the specs are processed before any user plugins.
-
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
-  -- recipes
-  { import = "astrocommunity.recipes.neovide" },
 
-  -- packs
+  -- pack
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.docker" },
 
-  -- media
-  { import = "astrocommunity.media.image-nvim" },
-
   -- colorscheme
-  { import = "astrocommunity.colorscheme.dracula-nvim" },
   { import = "astrocommunity.colorscheme.catppuccin" },
 
-  -- completions
-  -- { import = "astrocommunity.completion.codeium-nvim" },
-  -- { import = "astrocommunity.completion.supermaven-nvim" },
-  -- { import = "astrocommunity.completion.tabby-nvim" },
-
   -- other
+  { import = "astrocommunity.recipes.neovide" },
   { import = "astrocommunity.test.neotest" },
   { import = "astrocommunity.media.vim-wakatime" },
+  { import = "astrocommunity.media.image-nvim" },
   { import = "astrocommunity.markdown-and-latex.markview-nvim" },
   { import = "astrocommunity.code-runner.sniprun" },
 }
