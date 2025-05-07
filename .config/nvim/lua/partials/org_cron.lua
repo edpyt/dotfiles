d3a67e892f4b1ba5b0e8da22a950ce1552323a25
edpyt <edrsa1321s@gmail.com>
2025-05-07 16:00:37 +0500

    refactor(gtklock): change text color

--- .config/nvim/lua/partials/org_cron.lua
+++ .config/nvim/lua/partials/org_cron.lua
@@ -2,7 +2,7 @@ local orgmode = vim.fn.stdpath "data" .. "/lazy/orgmode"
 vim.opt.runtimepath:append(orgmode)
 
 require("orgmode").cron {
-  org_agenda_files = "~/orgfiles/**/*",
+  org_agenda_files = "~/Documents/orgfiles/**/*",
   notifications = {
     reminder_time = { 0, 5, 10 },
   },
