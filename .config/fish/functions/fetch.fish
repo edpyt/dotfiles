a9135e91ce6c5fa5de41cc6c8c369473b89f017b
edpyt <edrsa1321s@gmail.com>
2025-06-16 22:39:30 +0500

    feat: add anifetch

--- .config/fish/functions/fetch.fish
+++ .config/fish/functions/fetch.fish
@@ -1,7 +1,8 @@
-function fetch --wraps=fastfetch --description 'alias fetch=fastfetch'
-    if type -f fastfetch &>/dev/null
-        fastfetch $argv
-    else
-        missing_package fastfetch
-    end
-end
+alias fetch='python ~/.config/anifetch/anifetch.py ~/wallpapers/example.mp4 -r 10 -W 40 -H 20 -c "--symbols wide --fg-only" -ff'
+# function fetch --wraps=fastfetch --description 'alias fetch=fastfetch'
+#     if type -f fastfetch &>/dev/null
+#         fastfetch $argv
+#     else
+#         missing_package fastfetch
+#     end
+# end
