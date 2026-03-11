274139ffec413ccc7f77492f3ac26ec63dbadae5
edpyt <edrsa1321s@gmail.com>
2026-03-11 17:27:01 +0500

    feat(noctalia): use matugen generated colorscheme

--- .config/nvim/lua/functions/matugen.lua
+++ .config/nvim/lua/functions/matugen.lua
@@ -3,24 +3,24 @@ local M = {}
 function M.setup()
   require("base16-colorscheme").setup {
     -- Background tones
-    base00 = "#282828", -- Default Background
-    base01 = "#3c3836", -- Lighter Background (status bars)
-    base02 = "#474240", -- Selection Background
-    base03 = "#786f6b", -- Comments, Invisibles
+    base00 = "#082335", -- Default Background
+    base01 = "#0f3a57", -- Lighter Background (status bars)
+    base02 = "#0c3450", -- Selection Background
+    base03 = "#606d75", -- Comments, Invisibles
     -- Foreground tones
-    base04 = "#ebdbb2", -- Dark Foreground (status bars)
-    base05 = "#fbf1c7", -- Default Foreground
-    base06 = "#fbf1c7", -- Light Foreground
-    base07 = "#fbf1c7", -- Lightest Foreground
+    base04 = "#afb3b6", -- Dark Foreground (status bars)
+    base05 = "#f2f2f3", -- Default Foreground
+    base06 = "#f2f2f3", -- Light Foreground
+    base07 = "#f2f2f3", -- Lightest Foreground
     -- Accent colors
-    base08 = "#fb4934", -- Variables, XML Tags, Errors
-    base09 = "#83a598", -- Integers, Constants
-    base0A = "#fabd2f", -- Classes, Search Background
-    base0B = "#b8bb26", -- Strings, Diff Inserted
-    base0C = "#96e9c9", -- Regex, Escape Chars
-    base0D = "#e8e995", -- Functions, Methods
-    base0E = "#fcd782", -- Keywords, Storage
-    base0F = "#7d0d00", -- Deprecated, Embedded Tags
+    base08 = "#fd4663", -- Variables, XML Tags, Errors
+    base09 = "#98cc66", -- Integers, Constants
+    base0A = "#4f5de3", -- Classes, Search Background
+    base0B = "#65b2e7", -- Strings, Diff Inserted
+    base0C = "#bee996", -- Regex, Escape Chars
+    base0D = "#91c8ee", -- Functions, Methods
+    base0E = "#919aee", -- Keywords, Storage
+    base0F = "#900017", -- Deprecated, Embedded Tags
   }
 end
 
