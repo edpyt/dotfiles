b29dd19121995eb5c01cb2b66c7cf3ef003fcad1
edpyt <edrsa1321s@gmail.com>
2026-03-31 19:38:03 +0500

    feat(nvim): upgrade `astronvim` v6.0

--- .config/nvim/lua/functions/matugen.lua
+++ .config/nvim/lua/functions/matugen.lua
@@ -3,24 +3,24 @@ local M = {}
 function M.setup()
   require("base16-colorscheme").setup {
     -- Background tones
-    base00 = "#1f1f28", -- Default Background
-    base01 = "#2a2a37", -- Lighter Background (status bars)
-    base02 = "#333343", -- Selection Background
-    base03 = "#676785", -- Comments, Invisibles
+    base00 = "#1b1c22", -- Default Background
+    base01 = "#2d2e39", -- Lighter Background (status bars)
+    base02 = "#292933", -- Selection Background
+    base03 = "#64667a", -- Comments, Invisibles
     -- Foreground tones
-    base04 = "#717c7c", -- Dark Foreground (status bars)
-    base05 = "#c8c093", -- Default Foreground
-    base06 = "#c8c093", -- Light Foreground
-    base07 = "#c8c093", -- Lightest Foreground
+    base04 = "#afafb6", -- Dark Foreground (status bars)
+    base05 = "#f2f2f3", -- Default Foreground
+    base06 = "#f2f2f3", -- Light Foreground
+    base07 = "#f2f2f3", -- Lightest Foreground
     -- Accent colors
-    base08 = "#c34043", -- Variables, XML Tags, Errors
-    base09 = "#7e9cd8", -- Integers, Constants
-    base0A = "#c0a36e", -- Classes, Search Background
-    base0B = "#76946a", -- Strings, Diff Inserted
-    base0C = "#96b1e9", -- Regex, Escape Chars
-    base0D = "#ade996", -- Functions, Methods
-    base0E = "#e9cb96", -- Keywords, Storage
-    base0F = "#430d0e", -- Deprecated, Embedded Tags
+    base08 = "#fd4663", -- Variables, XML Tags, Errors
+    base09 = "#c566cc", -- Integers, Constants
+    base0A = "#8d5cd6", -- Classes, Search Background
+    base0B = "#6770e4", -- Strings, Diff Inserted
+    base0C = "#e396e9", -- Regex, Escape Chars
+    base0D = "#9399ec", -- Functions, Methods
+    base0E = "#b796e9", -- Keywords, Storage
+    base0F = "#900017", -- Deprecated, Embedded Tags
   }
 end
 
