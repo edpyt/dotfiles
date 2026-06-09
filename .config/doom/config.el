dc0b68db5abb2ac3eda9962a2fc6fc2728cbae75
edpyt <edrsa1321s@gmail.com>
2026-06-09 11:47:05 +0500

    chore: add new noctalia stuff

---
+++ .config/doom/config.el
@@ -0,0 +1,146 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Documents/orgfiles")
+(setq org-agenda-files `(,org-directory))
+(after! org
+        (setq org-todo-keywords
+              '((sequence "TODO(t)" "WAIT(w)" "|" "DONE(d)" "DELEGATED(g)"))))
+
+(setq org-icalendar-timezone "Asia/Yekaterinburg")
+(setq org-icalendar-use-scheduled '(event-if-todo event-if-not-todo))
+(setq org-icalendar-use-deadline '(event-if-todo event-if-not-todo))
+(setq org-icalendar-date-time-format ":%Y%m%dT%H%M%SZ")
+(setq org-icalendar-use-timezone-for-event t)
+(setq org-icalendar-todo-to-event t)
+(setq org-icalendar-include-todo 'all)
+(setq org-icalendar-export-ignore-warnings t)
+
+; FIXME: 
+; (require 'org-caldav)
+; (setq org-caldav-url "http://localhost:5232/edpyt/")
+; (setq org-caldav-calendar-id "org")
+; (setq org-caldav-sync-direction 'org->cal)
+; (setq org-caldav-inbox nil)
+; (setq org-caldav-sync-todo t)
+; (setq org-caldav-todo-percent-states
+;       '((0 "TODO") (0 "WAIT") (100 "DELEGATED") (100 "DONE")))
+; (setq org-caldav-files '("~/Documents/orgfiles/todos.org" "~/Documents/orgfiles/uni.org" "~/Documents/orgfiles/work.org"))
+
+;; -------------------
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `with-eval-after-load' block, otherwise Doom's defaults may override your
+;; settings. E.g.
+;;
+;;   (with-eval-after-load 'PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look them up).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+
+;;; ===== HABITS API TOGGLE =====
+(defun my/toggle-habit-state (habit-name date-str)
+  "Toggle DONE state for HABIT-NAME on DATE-STR."
+  (let ((org-file "/home/edpyt/Documents/orgfiles/habits.org"))
+    (find-file org-file)
+    (goto-char (point-min))
+    ;; Ищем заголовок. .* позволяет игнорировать TODO и теги
+    (when (re-search-forward (concat "^\\*+ .*" (regexp-quote habit-name)) nil t)
+      (org-back-to-heading t)
+      (let* ((subtree-start (point))
+             (subtree-end (save-excursion (org-end-of-subtree t t)))
+             ;; Ищем строку с State "DONE" и нужной датой (игнорируем время)
+             (search-pattern (concat "State \"DONE\".*\\[" date-str))
+             (found nil))
+
+        (save-excursion
+          (when (re-search-forward search-pattern subtree-end t)
+            (setq found (point))))
+
+        (if found
+          ;; Если нашли — удаляем всю строку
+          (progn
+            (goto-char found)
+            (beginning-of-line)
+            (let ((beg (point)))
+              (end-of-line)
+              (delete-region beg (point))
+              (when (looking-at "\n") (delete-char 1))))
+
+          ;; Если не нашли — вставляем новую запись
+          (progn
+            (goto-char subtree-start)
+            (org-end-of-meta-data t) ;; Прыгаем после SCHEDULED и PROPERTIES
+
+            ;; Подсматриваем отступ у существующих пунктов
+            (let ((indent "  "))
+              (save-excursion
+                (when (re-search-forward "^[ \t]+- " subtree-end t)
+                  (let ((match (match-string 0)))
+                    (setq indent (substring match 0 (- (length match) 2))))))
+
+              (unless (bolp) (insert "\n"))
+              (insert indent "- State \"DONE\"       from \"TODO\"       [" date-str "]\n"))))
+
+        (save-buffer)
+        (org-html-export-to-html)
+        (kill-buffer)))))
